d518437e77812730da9481e3e9c266b31a9323ad
Dmitri Tikhonov <dtikhonov@litespeedtech.com>
2020-02-14 10:54:23 -0500

    Add Travis CI

--- tut.c
+++ tut.c
@@ -1,9 +1,22 @@
+/* Copyright (c) 2020 LiteSpeed Technologies */
+/*
+ * tut.c is the example program to illustrate lsquic API usage.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "lsquic.h"
 
 int
 main (void)
 {
-    lsquic_global_init(LSQUIC_GLOBAL_SERVER|LSQUIC_GLOBAL_CLIENT);
+    if (0 != lsquic_global_init(LSQUIC_GLOBAL_SERVER|LSQUIC_GLOBAL_CLIENT))
+    {
+        fprintf(stderr, "global initialization failed\n");
+        exit(EXIT_FAILURE);
+    }
+
     lsquic_global_cleanup();
-    return 0;
+    exit(EXIT_SUCCESS);
 }
